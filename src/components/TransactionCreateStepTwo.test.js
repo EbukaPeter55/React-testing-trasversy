03a02af99b5ccdb3183d1074688b0d100ad1fa53
ebuka peter <peterdbrainy5@gmail.com>
2022-03-24 15:00:26 +0100

    A second test to check for user interactions and conditional rendering. Checking whether the pay button becomes enabled once amount and note is entered

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -1,9 +1,21 @@
 import { render, screen } from "@testing-library/react"
 import TransactionCreateStepTwo from "./TransactionCreateStepTwo"
+import userEvent from "@testing-library/user-event";
+
 
 test('on initial render, the pay button is disabled', async ()=> {
     render(<TransactionCreateStepTwo sender={{id: '5'}} receiver={{id: '5'}}/>)
 
     // First assertion
     expect(await screen.findByRole('button', { name: /pay/i })).toBeDisabled();
+});
+
+test('if an amount and note is entered, the pay button becomes enabled', async ()=> {
+    render(<TransactionCreateStepTwo sender={{id: '5'}} receiver={{id: '5'}}/>)
+
+    // First assertion
+    userEvent.type(screen.getByPlaceholderText(/amount/i), "50")
+    userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner")
+    
+    expect(await screen.findByRole('button', { name: /pay/i })).toBeEnabled();
 });
