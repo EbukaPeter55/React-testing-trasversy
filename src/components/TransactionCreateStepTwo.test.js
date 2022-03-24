ed5e7a85f7c11274134aedd400419af66b15815f
ebuka peter <peterdbrainy5@gmail.com>
2022-03-24 15:35:30 +0100

    Combine the two unit test into a single integration test

--- src/components/TransactionCreateStepTwo.test.js
+++ src/components/TransactionCreateStepTwo.test.js
@@ -3,19 +3,20 @@ import TransactionCreateStepTwo from "./TransactionCreateStepTwo"
 import userEvent from "@testing-library/user-event";
 
 
-test('on initial render, the pay button is disabled', async ()=> {
-    render(<TransactionCreateStepTwo sender={{id: '5'}} receiver={{id: '5'}}/>)
 
-    // First assertion
-    expect(await screen.findByRole('button', { name: /pay/i })).toBeDisabled();
-});
-
+// INTEGRATION TEST INVOLVES TESTING A MULTIPLE UNIT OF CODE TOGETHER
 test('if an amount and note is entered, the pay button becomes enabled', async ()=> {
     render(<TransactionCreateStepTwo sender={{id: '5'}} receiver={{id: '5'}}/>)
 
     // First assertion
+    expect(await screen.findByRole('button', { name: /pay/i })).toBeDisabled();
+    // First assertion
     userEvent.type(screen.getByPlaceholderText(/amount/i), "50")
     userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner")
     
+    // Second assertion
     expect(await screen.findByRole('button', { name: /pay/i })).toBeEnabled();
 });
+
+
+
